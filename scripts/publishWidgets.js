5a534826e0bbb6e77a2fcb698e2b6a406cd098e1
nitish.mittal <nitish.mittal@wingify.com>
2018-11-02 12:07:46 +0530

    update

--- scripts/publishWidgets.js
+++ scripts/publishWidgets.js
@@ -1,2 +1,7 @@
 const execSync = require('child_process').execSync;
 
+let output = execSync(`lerna changed --loglevel silent --json`);
+let changes = JSON.parse(output);
+
+//npm unpublish --force --registry https://npm.wingify.com @wingify/${packageName}
+//lerna publish --registry http://localhost:4873/
