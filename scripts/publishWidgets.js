3cdb166c55cf45566156a890a2397cbab3c0581c
nitish.mittal <nitish.mittal@wingify.com>
2018-11-02 12:56:54 +0530

    update

--- scripts/publishWidgets.js
+++ scripts/publishWidgets.js
@@ -5,3 +5,6 @@ let changes = JSON.parse(output);
 
 //npm unpublish --force --registry https://npm.wingify.com @wingify/${packageName}
 //lerna publish --registry http://localhost:4873/
+///lerna publish patch --registry http://localhost:4873/ --yes
+
+//npm unpublish --force --registry http://localhost:4873/ floating-bar@1.0.3
