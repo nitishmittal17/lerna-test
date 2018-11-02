1a084b16f2a7d065197596f0b9f5eaccc467ece1
nitish.mittal <nitish.mittal@wingify.com>
2018-11-02 15:06:29 +0530

    update

--- scripts/publishWidgets.js
+++ scripts/publishWidgets.js
@@ -2,6 +2,7 @@ const execSync = require('child_process').execSync;
 
 let output = execSync(`lerna changed --loglevel silent --json`);
 let changes = JSON.parse(output);
+console.log(changes);
 
 //npm unpublish --force --registry https://npm.wingify.com @wingify/${packageName}
 //lerna publish --registry http://localhost:4873/
@@ -11,10 +12,8 @@ let changes = JSON.parse(output);
 //Remove local tag - git tag -d 12345
 //Remove remote tag - git push --delete origin tagName
 
-try {
-	output = execSync(`lerna publish patch --registry http://localhost:4873/ --loglevel silent --yes`);
-	console.log(output.toString());
-} catch(e) {
-	console.log('Error');
-	console.log(e);
-}
+execSync(`lerna publish patch --registry http://localhost:4873/ --loglevel silent --yes`);
+
+output = execSync(`lerna changed --loglevel silent --json`);
+changes = JSON.parse(output);
+console.log(changes);
